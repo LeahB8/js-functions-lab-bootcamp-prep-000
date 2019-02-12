ce5721fd5fcc70b8b958eea83835eb82b7acd247
Leah Boyd <leahboyd08@gmail.com>
2019-02-12 17:07:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ function happyHolidays(){
   return (`Happy holidays!`)
 }
 
-
+happyHolidays();
 
 function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
   var name = "Shemar"
 }
 
-
-
-  
+happyHolidaysTo(name);
 
 function happyHolidayTo(holiday, name){
   return (`Happy ${holiday}, ${name}!`)
@@ -20,14 +18,12 @@ function happyHolidayTo(holiday, name){
   var holiday = "Valentine's Day"
 }
 
-
-
-  var holiday = "Valentine's Day"
-  var days = 4
-
+happyHolidaysTo(holiday, name);
 
 function holidayCountdown(days, holiday){
   return (`It's ${days} days until ${holiday}!`)
+  var holiday = "Valentine's Day"
+  var days = 4
 }
 
-
+holidayCountdown(days, holiday);
