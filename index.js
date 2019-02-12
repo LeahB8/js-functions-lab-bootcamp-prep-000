e99dde97100ca3953a4910af841e97800078d21c
Leah Boyd <leahboyd08@gmail.com>
2019-02-12 16:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// write your code below!
+
+function happyHolidays(){
+  console.log (`Happy holidays!`)
+}
+
+happyHolidays
+
+var name = "Shemar"
+
+function happyHolidaysTo(name){
+  console.log (`Happy holidays, ${name}!`)
+}
+
+happyHolidaysTo("Shemar")
+
+  var name = "Shemar"
+  var holiday = "Valentine's Day"
+
+function happyHolidayTo(holiday, name){
+  console.log (`Happy ${holiday}, ${name}!`)
+}
+
+happyHolidaysTo("Valentine's Day", "Shemar")
+
+  var holiday = "Valentine's Day"
+  var days = 4
+
+
+function holidayCountdown(days, holiday){
+  console.log (`It's ${days} days until ${holiday}!`)
+}
+
+holidayCountdown(4, "Valentine's Day")
